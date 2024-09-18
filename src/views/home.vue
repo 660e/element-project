21176b87def02eab98a1ee34b4a480aa29d538c2
Min <solunar@vip.qq.com>
2024-09-18 21:57:23 +0800

    Update

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <el-button type="primary">home</el-button>
+  <div class="h-screen flex justify-center items-center">
+    <div>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
   </div>
 </template>
