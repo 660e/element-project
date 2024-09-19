ea7c34b898a0f7ff35688878cea8dc39ab7c7049
Min <solunar@vip.qq.com>
2024-09-19 22:32:47 +0800

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -9,15 +9,19 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // https://github.com/unplugin/unplugin-auto-import
     AutoImport({
       dts: './src/auto-imports.d.ts',
       imports: ['vue', 'vue-router'],
       resolvers: [ElementPlusResolver()],
     }),
+
+    // https://github.com/unplugin/unplugin-vue-components
     Components({
       dts: './src/components.d.ts',
       resolvers: [ElementPlusResolver()],
     }),
+
     vue(),
     vueJsx(),
   ],
