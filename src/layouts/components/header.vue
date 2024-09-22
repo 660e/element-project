bbf94b2e42e2571761924b3b4cee01c89c8b4a87
Min <solunar@vip.qq.com>
2024-09-22 19:29:22 +0800

    Update

--- src/layouts/components/header.vue
+++ src/layouts/components/header.vue
@@ -3,7 +3,9 @@ import ToggleDark from './toggle-dark.vue';
 </script>
 
 <template>
-  <header class="p-2 flex space-x-2 bg-neutral-200 dark:bg-neutral-800">
+  <header class="p-2 flex items-center space-x-2 bg-neutral-200 dark:bg-neutral-800">
+    <img src="/favicon.ico" />
+    <div class="text-xl leading-none">Lorem ipsum dolor sit amet.</div>
     <div class="flex-1"></div>
     <toggle-dark />
     <el-avatar :size="32" src="https://avatars.githubusercontent.com/u/28943787" />
