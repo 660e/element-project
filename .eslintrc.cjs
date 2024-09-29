0d0f59261b4507f6649b54ab0a9d28af4ba0fbbc
Min <solunar@vip.qq.com>
2024-09-29 21:06:11 +0800

    Update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,14 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting'],
+  // extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier/skip-formatting'],
+  extends: ['@vue/eslint-config-typescript', 'plugin:vue/vue3-recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+
+    'vue/multi-word-component-names': 'off',
   },
 };
