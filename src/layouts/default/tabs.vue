1aee2f55ef56cd8586a3e6b43e73c4135123a450
Min <solunar@vip.qq.com>
2024-09-30 21:16:27 +0800

    Update

---
+++ src/layouts/default/tabs.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { TabPaneName } from 'element-plus';
+
+const active = ref('Tab 1');
+const tabs = ref(['Tab 1', 'Tab 2', 'Tab 3', 'Tab 4']);
+const tabRemove = (name: TabPaneName) => {
+  console.log(name);
+};
+</script>
+
+<template>
+  <div class="app-tabs">
+    <el-tabs v-model="active" type="border-card" closable @tab-remove="tabRemove">
+      <el-tab-pane v-for="tab in tabs" :key="tab" :label="tab" :name="tab" />
+    </el-tabs>
+  </div>
+</template>
+
+<style lang="scss">
+.app-tabs > .el-tabs {
+  background-color: transparent;
+  border: 0;
+  .el-tabs__header {
+    background-color: transparent;
+    .el-tabs__item {
+      line-height: 1;
+      &.is-active {
+        background-color: theme('colors.neutral.100');
+      }
+    }
+  }
+  .el-tabs__content {
+    display: none;
+  }
+}
+.dark .app-tabs > .el-tabs > .el-tabs__header .el-tabs__item.is-active {
+  background-color: theme('colors.black');
+}
+</style>
