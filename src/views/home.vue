fa5c1ce2215c1374be89d1824df142cbd4e11ead
Min <solunar@vip.qq.com>
2024-10-13 14:39:38 +0800

    Update

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,43 +1,11 @@
-<script setup lang="ts">
-const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-];
-
-const tableRef = ref();
-
-onMounted(() => {
-  console.log(tableRef.value);
-  console.log(tableRef.value.$el);
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <q-page>
     <div class="h-full p-2">
-      <q-table ref="tableRef" :data="tableData" class="bg-red-500" style="color: blue" border>
-        <el-table-column prop="date" label="Date" />
-        <el-table-column prop="name" label="Name" />
-        <el-table-column prop="address" label="Address" />
-      </q-table>
+      <div class="flex">
+        <img src="@/assets/tg-silence1e9c5d.jpg" />
+      </div>
     </div>
   </q-page>
 </template>
