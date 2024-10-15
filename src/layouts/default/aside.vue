4d3498b3d20eadaed64de78e4a57ac34c9d828fd
Min <solunar@vip.qq.com>
2024-10-15 22:01:11 +0800

    Update

--- src/layouts/default/aside.vue
+++ src/layouts/default/aside.vue
@@ -1,53 +1,45 @@
 <script setup lang="tsx">
 import type { RouteRecordRaw } from 'vue-router';
-import { routes } from '@/router';
+import { routes as routesRaw } from '@/router';
 
 const isCollapse = ref(false);
 
+const renderItem = (routes: RouteRecordRaw[]) => {
+  return routes.map((route, index) => {
+    if (route.children?.length) {
+      return (
+        <el-sub-menu index={String(index)}>
+          {{
+            title: () => {
+              return (
+                <>
+                  <el-icon>
+                    <i-ep-menu />
+                  </el-icon>
+                  <span>{route.meta?.label}</span>
+                </>
+              );
+            },
+            default: () => renderItem(route.children || []),
+          }}
+        </el-sub-menu>
+      );
+    } else {
+      return (
+        <el-menu-item index={String(index)}>
+          <el-icon>
+            <i-ep-menu />
+          </el-icon>
+          <span>{route.meta?.label}</span>
+        </el-menu-item>
+      );
+    }
+  });
+};
 const renderMenu = (props: { routes: RouteRecordRaw[] }) => {
   return (
     <el-menu collapse={isCollapse.value} class="w-60">
-      {props.routes.map((route, index) => {
-        if (route.children?.length) {
-          return (
-            <el-sub-menu index={String(index)}>
-              {{
-                title: () => {
-                  return (
-                    <>
-                      <el-icon>
-                        <i-ep-menu />
-                      </el-icon>
-                      <span>{route.meta?.label}</span>
-                    </>
-                  );
-                },
-                default: () => {
-                  return route.children?.map((r, i) => {
-                    return (
-                      <el-menu-item index={String(i)}>
-                        <el-icon>
-                          <i-ep-menu />
-                        </el-icon>
-                        <span>{r.meta?.label}</span>
-                      </el-menu-item>
-                    );
-                  });
-                },
-              }}
-            </el-sub-menu>
-          );
-        } else {
-          return (
-            <el-menu-item index={String(index)}>
-              <el-icon>
-                <i-ep-menu />
-              </el-icon>
-              <span>{route.meta?.label}</span>
-            </el-menu-item>
-          );
-        }
-      })}
+      {renderItem(props.routes)}
     </el-menu>
   );
 };
@@ -56,7 +48,7 @@ const renderMenu = (props: { routes: RouteRecordRaw[] }) => {
 <template>
   <aside class="flex flex-col border-r border-neutral-200 dark:border-neutral-800">
     <el-scrollbar class="flex-1">
-      <render-menu :routes="routes" />
+      <render-menu :routes="routesRaw" />
     </el-scrollbar>
 
     <div
